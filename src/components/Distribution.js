24dd56f6d5df24b3594e7e42f4be5a01575399c1
Mbabazi-Rightman22 <rightmanmbabazi@gmail.com>
2023-04-28 11:04:49 +0200

    add download

--- src/components/Distribution.js
+++ src/components/Distribution.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Modal from "../Modal/Modal";
+import { CSVLink } from "react-csv";
 
 const Distribution = () => {
   const [features, setFeature] = useState([]);
@@ -47,22 +48,35 @@ const Distribution = () => {
               value={searchText}
               onChange={(e) => setsearchText(e.target.value)}
               className="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Recherche Numero de Colis"
+              placeholder="Search Reg number"
             />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 9.75v6.75m0 0l-3-3m3 3l3-3m-8.25 6a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
-              />
-            </svg>
+            
+
+            <CSVLink filename={`Student-${new Date().toISOString()}.csv`} className="cursor-pointer" data={
+                    features
+                      .filter((feature) =>
+                        (feature.attributes.registration_number + "").startsWith(
+                          searchText
+                        )
+                      ).map(f => (
+                        { ...f.attributes }
+                      ))
+                  }>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 9.75v6.75m0 0l-3-3m3 3l3-3m-8.25 6a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z"
+                      />
+                    </svg>
+                  </CSVLink>
           </div>
           <div className="overflow-scroll">
             <table
